54260e559c90e6b191e0641b8bfe1e4abc0ba1de
Graham <Grahamf065@deltalearns.ca>
2019-09-23 11:18:54 -0700

    Create JS Practice2.js

---
+++ JS Practice2.js
@@ -0,0 +1,14 @@
+//1 canadian dollar = to .75 united states.//
+//C=canadian currency, U=US currency, F=euros, k= chinese currency
+var c = 100
+let U = c * 0.75
+let F = c * 0.69
+let k = c * 5.36
+console.log("if i have $100 Canadian how much US, euros or Yuan would i have?")
+console.log( "Canadian to US would be $100 x 0.75, so c * 0.75" ) 
+console.log(U)
+console.log("$100 Canadian to euros, c * 0.69") 
+console.log(F)
+console.log("$100 canadian to yuans, c * 5.36") 
+console.log(k)
+//this code can help you quickly find the the correct currency wherever you are//
